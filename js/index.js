e260b1e7fa7f7dc5198548b1efeafac8b3e48790
연진주 <jinjuyeon3@gmail.com>
2026-04-14 16:51:16 +0900

    애니메이션 추가

--- js/index.js
+++ js/index.js
@@ -1,20 +1,5 @@
 document.addEventListener("DOMContentLoaded",()=>{
-    /* =========================
-       1. 스크롤 애니메이션
-    ========================= */
-    const elements = document.querySelectorAll('.fade-up, .fade-left, .fade-right, .fade-show');
-
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('active');
-            }
-        });
-    });
-
-    elements.forEach(el => observer.observe(el));
-
-
+    
     /* =========================
        2. 스크롤 애니메이션 (위로 이동)
     ========================= */
